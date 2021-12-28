38afd47bfda70a6331d85bcbbbd2a5fbfe917c0d
GGORG <ggorg@onet.pl>
2021-12-28 12:12:31 +0100

    added folder removing to runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,7 @@
 run() {
-    echo "[*] Making sure Git is installed"
+    echo "[*] Making sure there is no archix folder..."
+    rm -rf archix
+    echo "[*] Making sure Git is installed..."
     pacman -Sy --noconfirm --needed git
     echo "[*] Cloning the repository..."
     git clone https://github.com/GGORG0/archix.git
@@ -7,5 +9,6 @@ run() {
     echo "[*] Starting Archix..."
     chmod +x *.sh
     ./archix.sh
+    cd ..
 }
 run
