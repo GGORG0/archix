976cd4b02cc80c8b95e8a2c2b7e9e2ed954765a8
GGORG <ggorg@onet.pl>
2021-12-28 11:14:58 +0100

    added git installation to runner.sh

--- runner.sh
+++ runner.sh
@@ -1,4 +1,6 @@
 run() {
+    echo "[*] Making sure Git is installed"
+    pacman -S --noconrim --needed git
     echo "[*] Cloning the repository..."
     git clone https://github.com/GGORG0/archer.git
     cd archer
