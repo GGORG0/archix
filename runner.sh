888b61f0b02ff84e89fbdf6e7d507f124693f2d2
GGORG <ggorg@onet.pl>
2021-12-28 11:31:19 +0100

    fixed pacman in runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 run() {
     echo "[*] Making sure Git is installed"
-    pacman -S --noconrim --needed git
+    pacman -S --noconfirm --needed git
     echo "[*] Cloning the repository..."
     git clone https://github.com/GGORG0/archer.git
     cd archer
