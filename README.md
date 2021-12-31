297267bab04694200867e4119edfd75825ce58f7
GGORG <ggorg@onet.pl>
2021-12-31 15:12:56 +0100

    renamed the git cloned folder to archix.git for a simpler command

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Archix is a simple installer for Arch Linux
 To run it, simply boot an Arch Linux installer ISO and run the following commands:
 
 ```bash
-curl -Lo archix.sh "https://git.io/archix"
-bash archix.sh
+curl -LO "https://git.io/archix"
+bash archix
 ```
--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
 run() {
-    echo "[*] Making sure there is no archix folder..."
-    rm -rf archix
+    echo "[*] Making sure there is no archix.git folder..."
+    rm -rf archix.git
     echo "[*] Making sure Git is installed..."
     pacman -Sy --noconfirm --needed git
     echo "[*] Cloning the repository..."
-    git clone https://github.com/GGORG0/archix.git
-    cd archix
+    git clone https://github.com/GGORG0/archix.git archix.git
+    cd archix.git
     echo "[*] Starting Archix..."
     chmod +x *.sh
     ./archix.sh
